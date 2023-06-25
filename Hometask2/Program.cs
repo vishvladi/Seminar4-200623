039b0b58243b16507559b0a4564cb484bc1cfefa
vishvladi <vlad-vishnevski@mail.ru>
2023-06-26 01:24:21 +0300

    сумма цифр в числе

---
+++ Hometask2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+int ReadInt(string message)
+ {
+    System.Console.Write(message);
+    int inputedNum = Convert.ToInt32(Console.ReadLine());
+    return inputedNum;
+}
+int SumDigitsInnum(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum += number % 10;
+        number = number / 10;
+    }
+    return sum;
+}
+int num = ReadInt("Введите число: ");
+Console.WriteLine($"Сумма всех цифр в числе {num} равна { SumDigitsInnum(num)} ");
